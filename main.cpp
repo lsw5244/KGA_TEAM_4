0390075131abddf027b6115bbc881fa95bf29800
LEW <lsw5244@naver.com>
2021-09-28 19:37:06 +0900

    fix : 콘솔창이 더이상 출력되지 않도록 수정
    콘솔창이 출력되지 않도록 수정

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <Windows.h>
 #include "MainGame.h"
-
-#ifdef UNICODE
-#pragma comment(linker, "/entry:wWinMainCRTStartup /subsystem:console")
-#else
-#pragma comment(linker, "/entry:WinMainCRTStartup /subsystem:console")
-#endif
+//
+//#ifdef UNICODE
+//#pragma comment(linker, "/entry:wWinMainCRTStartup /subsystem:console")
+//#else
+//#pragma comment(linker, "/entry:WinMainCRTStartup /subsystem:console")
+//#endif
 
 // 전역변수
 HINSTANCE	g_hInstance;
